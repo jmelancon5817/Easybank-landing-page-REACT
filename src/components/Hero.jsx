dd3b29ffd2df7e4a5381cbfd2d622bbb7cb812b4
Jacob Melancon <jmelancon5817@bellsouth.net>
2022-12-07 08:48:01 -0600

    Update Hero Component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,26 @@
+import "../styles/styles.css";
+
 function Hero() {
-  return <div className="Hero">Hero</div>;
+  return (
+    <div>
+      <section className="hero">
+        <div className="container">
+          <div className="hero_image"></div>
+          <div className="hero_text conatiner--pall">
+            <h1>Next generation digital banking</h1>
+            <p>
+              Take your financial life online. Your Easybank account will be a
+              one-stop-shop for spending, saving, budgeting, investing, and much
+              more.
+            </p>
+            <button className="hero_button" type="button">
+              Request Invite
+            </button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default Hero;
